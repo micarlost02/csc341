cc741031380f57fe217a1a050e3c7900575b483d
micarlost <cmaldonado41@toromail.csudh.edu>
2024-09-12 16:58:51 -0700

    Eddied Lab 4 code

--- cmaldonado4.c
+++ cmaldonado4.c
@@ -15,16 +15,8 @@ int main(int argc, char *argv[])
    values = (int *)malloc(sizeof(int)*5);
    int *pointer1 = &argc;
    printf("The biggest address is argc with %p\n",&pointer1);
-   for(i = 0; i < 5; i++)
-   {
-	values[i] = i;
-   }
-   printf("This is the address of each stack variable of values\n");
-   for(i = 0; i < 5; i++)
-   {
-	printf("values[%d] = %d with address of %p\n", i, values[i], &values[i]);
-   }
-   printf("The address of the heap is %p\n", &values);
+   printf("local variable of the stack address is %p\n", &i);
+   printf("The address of the heap is %p\n", values);
    printf("The address of the uninitialized variable is %p\n", &x);
    printf("The smallest address is initialized variable with %p\n", &y);
    return EXIT_SUCCESS;
